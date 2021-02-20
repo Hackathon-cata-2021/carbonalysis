318e9b6c4d9461442ecb379a09dc72f3a842e882
Amir Sharapov <asharapov@catalyte.io>
2021-02-20 16:33:08 -0600

    add code for the onchangehandler

--- carbonalysis/src/components/login/login.jsx
+++ carbonalysis/src/components/login/login.jsx
@@ -3,17 +3,26 @@ import { CarbonFootprintContext } from '../../context/CarbonFootprintContext'
 
 const Login = () => {
     const {user, setUser} = useContext(CarbonFootprintContext);
-    const submitHandler = () => {
-        
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const onChangeHandler = (e, type) => {
+        if (type == 'password') {
+            setPassword(prevPassword => prevPassword = e.target.value);
+        } else {
+            setEmail(prevEmail => prevEmail = e.target.value);
+        }
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
     }
     return(
         <div>
             <form>
                 <label>Email</label>
-                <input type="email"></input>
+                <input value={email} onChange = {(e) => onChangeHandler(e, "email")} type="email"></input>
                 <br/>
                 <label>Password</label>
-                <input type="password"></input>
+                <input value={password} onChange = {(e) => onChangeHandler(e, "password")} type="password"></input>
                 <br/>
                 <button type="submit">Submit</button>
             </form>
