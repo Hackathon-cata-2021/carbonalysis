d690139dcd23b4d43c6a7b4cba7c332663bdb9dd
Reyna Javar <rjavar@catalyte.io>
2021-02-20 15:35:45 -0800

    add login

--- carbonalysis/src/components/login/login.jsx
+++ carbonalysis/src/components/login/login.jsx
@@ -1,31 +1,52 @@
 import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom';
 import { carbonFootprintContext } from '../../context/CarbonFootprintContext'
 
 const Login = () => {
     const {user, setUser} = useContext(carbonFootprintContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const onChangeHandler = (e, type) => {
-        if (type == 'password') {
-            setPassword(prevPassword => prevPassword = e.target.value);
-        } else {
-            setEmail(prevEmail => prevEmail = e.target.value);
-        }
+
+    const [credentials, setCredentials] = useState({ username: '', password: ''});
+    const [invalidCredentials, setInvalidCredentials] = useState(false);
+
+    const history = useHistory();
+
+    const onChangeHandler = (event, input) => {
+        if (invalidCredentials) {
+            setInvalidCredentials(false);
+          }
+          setCredentials({ ...credentials, [input]: event.target.value });
     }
     const submitHandler = (e) => {
         e.preventDefault();
+
+        HttpHelper('/login', 'POST', credentials)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Invalid username or password');
+            })
+            .then((data) => {
+                sessionStorage.setItem('token', data.token);
+                setUser(data);
+                history.push('/dashboard');
+            })
+            .catch(() => {
+                setInvalidCredentials(true);
+            })
     }
     return(
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <label>Email</label>
-                <input value={email} onChange = {(e) => onChangeHandler(e, "email")} type="email"></input>
+                <input value={credentials.username} onChange = {(e) => onChangeHandler(e, "username")} type="email"></input>
                 <br/>
                 <label>Password</label>
-                <input value={password} onChange = {(e) => onChangeHandler(e, "password")} type="password"></input>
+                <input value={credentials.password} onChange = {(e) => onChangeHandler(e, "password")} type="password"></input>
                 <br/>
                 <button type="submit">Submit</button>
             </form>
+            {invalidCredentials && <p>Invalid username or password</p>}
         </div>
     )
 }
