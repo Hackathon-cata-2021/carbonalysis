a060fafff1b2e7158bb37b412074c2763a2db8cf
Amir Sharapov <asharapov@catalyte.io>
2021-02-20 16:13:55 -0600

    create login bare bones form

--- carbonalysis/src/components/login/login.jsx
+++ carbonalysis/src/components/login/login.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const login = () => {
     return(
         <div>
-            
+            <form>
+                <label>Username / Email</label>
+                <input type="text"></input>
+                <label>Password</label>
+                <input type="text"></input>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
