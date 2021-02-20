6f1d7a7c33ebc4b2398806086be0d0c69ac3afed
Reyna Javar <rjavar@catalyte.io>
2021-02-20 15:58:04 -0800

    WIP login

--- carbonalysis-api/src/main/java/com/github/carbonalysis/ApplicationRunner.java
+++ carbonalysis-api/src/main/java/com/github/carbonalysis/ApplicationRunner.java
@@ -4,12 +4,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApplicationRunner {
+
   private static Logger logger = LogManager.getLogger(ApplicationRunner.class);
 
   public static void main(String[] args) {
     SpringApplication.run(ApplicationRunner.class);
   }
+
 }
--- carbonalysis-api/src/main/java/com/github/carbonalysis/auth/AuthController.java
+++ carbonalysis-api/src/main/java/com/github/carbonalysis/auth/AuthController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class AuthController {
   @Autowired
   UsersService usersService;
