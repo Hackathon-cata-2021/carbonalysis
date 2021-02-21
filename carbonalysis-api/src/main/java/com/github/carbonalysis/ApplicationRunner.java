e84f185ca57a05a8264c74cbbd4969beacbbafe3
Darren Shin <hshin@catalyte.io>
2021-02-20 19:24:45 -0500

    bleh

--- carbonalysis-api/src/main/java/com/github/carbonalysis/ApplicationRunner.java
+++ carbonalysis-api/src/main/java/com/github/carbonalysis/ApplicationRunner.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApplicationRunner {
@@ -11,5 +14,17 @@ public class ApplicationRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ApplicationRunner.class);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("http://localhost:3000");
+      }
+    };
   }
 }
